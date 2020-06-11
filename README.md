9b44696a91145df6e46635042fb7f4bbe83bf6d1
ydennisy <dennis@mediaiqdigital.com>
2020-06-11 17:33:35 +0100

    chore: tsconfig setup for a monorepo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EdgeKit | `edkt();`
+
+[![lerna](https://img.shields.io/badge/maintained%20with-lerna-cc00ff.svg)](https://lerna.js.org/)
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
+import * as engine from '@edgekit/engine';
 import { getPageFeatures } from './features';
 import { setAndReturnAllPageViews, updateMatchedAudiences } from './storage';
 
@@ -10,14 +11,10 @@ interface IPageFeatureGetter {
   func: () => Promise<string[]>;
 }
 
-const check = (conditions, pageViews) => {
-
-}
-
 export const edkt = async (config: IConfig) => {
   const { pageFeatureGetters } = config;
   const pageFeatures = await getPageFeatures(pageFeatureGetters);
   const pageViews = setAndReturnAllPageViews(pageFeatures);
-  const matchedAudiences = check( ,pageViews);
+  const matchedAudiences = engine.check( ,pageViews);
   updateMatchedAudiences(matchedAudiences);
 }
