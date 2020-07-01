74a9781b0b3ba9abb12732d3ee42255ea18db66e
ydennisy <dennis@airgrid.io>
2020-07-01 13:36:37 +0100

    feat: added first two audience defintions #3

--- src/audiences/index.ts
+++ src/audiences/index.ts
@@ -1,4 +1,33 @@
-import interestTravel from './interest/travel';
-import interestSport from './interest/sport';
+// import interestTravel from './interest/travel';
+// import interestSport from './interest/sport';
+import sportKeywords from './interest/sport/keywords';
+import travelKeywords from './interest/travel/keywords';
 
-export default [interestTravel, interestSport];
+const TTL_IN_SECS = 100;
+const LOOKBACK_IN_SECS = 100;
+const OCCURRENCES = 3;
+
+export const sportInterestAudience = {
+  id: 209,
+  name: 'Interest | Sport',
+  ttl: TTL_IN_SECS,
+  lookback: LOOKBACK_IN_SECS,
+  occurrences: OCCURRENCES,
+  keywords: sportKeywords,
+};
+
+export const travelInterestAudience = {
+  id: 210,
+  name: 'Interest | Travel',
+  ttl: TTL_IN_SECS,
+  lookback: LOOKBACK_IN_SECS,
+  occurrences: OCCURRENCES,
+  keywords: travelKeywords,
+};
+
+export const allAudienceDefinitions = [
+  sportInterestAudience,
+  travelInterestAudience,
+];
+
+// export default [interestTravel, interestSport];
