1ccb096be452018fff8e6eaf8441bcdcbef2f8a4
Peter Kirkham <k11kirky@gmail.com>
2020-07-05 23:27:45 +0100

    test: fixed TTL test

--- test/run.test.ts
+++ test/run.test.ts
@@ -1,9 +1,16 @@
 import { edkt } from '../src';
 import { AudienceDefinition, PageView } from '../types';
 import { timeStampInSecs } from 'src/utils';
-import { viewStore } from 'src/store'
+import { viewStore, audienceStore } from 'src/store';
 
 const sportPageFeatureGetter = {
+  name: 'keywords',
+  func: (): Promise<string[]> => {
+    return Promise.resolve(['sport']);
+  },
+};
+
+const travelPageFeatureGetter = {
   name: 'keywords',
   func: (): Promise<string[]> => {
     return Promise.resolve(['sport']);
@@ -19,6 +26,15 @@ const sportAudience: AudienceDefinition = {
   lookback: 10,
   occurrences: 2,
   keywords: ['sport'],
+};
+
+const travelAudience: AudienceDefinition = {
+  id: 'travel_id',
+  name: 'Travel Audience',
+  ttl: TTL,
+  lookback: 10,
+  occurrences: 2,
+  keywords: ['travel'],
 };
 
 const ONE_SPORTS_PAGE_VIEW: Array<PageView> = [
@@ -40,6 +56,7 @@ const setUpLocalStorage = (pageViews: Array<PageView>) => {
   localStorage.setItem('edkt_page_views', JSON.stringify(pageViews));
   //We need to reload from local storage because its only done on construction
   viewStore._load();
+  audienceStore._load();
 };
 
 describe('Test edkt run', () => {
@@ -83,21 +100,22 @@ describe('Test edkt run', () => {
     expect(edktMatchedAudeinces.length).toEqual(1);
   });
 
-  it('does not with two sport page view after ttl', async () => {
+  it('does not match with two sport page view & one travel view after sport ttl', async () => {
     setUpLocalStorage(TWO_SPORTS_PAGE_VIEW_AFTER_TTL);
 
     await edkt.run({
-      pageFeatureGetters: [sportPageFeatureGetter],
-      audienceDefinitions: [sportAudience],
+      pageFeatureGetters: [travelPageFeatureGetter],
+      audienceDefinitions: [travelAudience],
     });
+
+    const edktMatchedAudeinces = edkt.getMatchedAudiences();
 
     const edktPageViews = JSON.parse(
       localStorage.getItem('edkt_page_views') || '[]'
     );
 
-    const edktMatchedAudeinces = JSON.parse(
-      localStorage.getItem('edkt_matched_audiences') || '[]'
-    );
+    console.log(edktPageViews);
+    console.log(edktMatchedAudeinces);
 
     expect(edktPageViews.length).toEqual(3);
     expect(edktMatchedAudeinces.length).toEqual(0);
