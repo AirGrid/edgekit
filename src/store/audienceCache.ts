9cab7cd1198d0580ec942f1567e2efed3f09f395
Peter Kirkham <k11kirky@gmail.com>
2020-07-08 21:43:38 +0100

    feat: add audience cache and audience version

---
+++ src/store/audienceCache.ts
@@ -0,0 +1,58 @@
+import { StorageKeys, AudienceDefinition, CachedAudienceMetaData } from 'types';
+import { storage, timeStampInSecs } from '../utils';
+
+class AudienceCacheStore {
+  cachedAudiences: AudienceDefinition[];
+  cachedAudiencesMetaData: CachedAudienceMetaData;
+
+  constructor() {
+    this.cachedAudiences = [];
+    this.cachedAudiencesMetaData = {
+      timeChecked: 0,
+      audiences: [],
+    };
+    this._load();
+  }
+
+  _load() {
+    const audiences: AudienceDefinition[] =
+      storage.get(StorageKeys.CACHED_AUDIENCES) || [];
+    const audiencesMetaData: CachedAudienceMetaData =
+      storage.get(StorageKeys.CACHED_AUDIENCE_META_DATA) || [];
+    this.cachedAudiences = audiences;
+    this.cachedAudiencesMetaData = audiencesMetaData;
+    this._save();
+  }
+
+  _save() {
+    storage.set(StorageKeys.CACHED_AUDIENCES, this.cachedAudiences);
+    storage.set(
+      StorageKeys.CACHED_AUDIENCE_META_DATA,
+      this.cachedAudiencesMetaData
+    );
+  }
+
+  setAudienceCache(audiencesToCache: AudienceDefinition[]) {
+    this.cachedAudiences = audiencesToCache;
+    this.cachedAudiencesMetaData = {
+      timeChecked: timeStampInSecs(),
+      audiences: audiencesToCache.map((audience) => {
+        return {
+          id: audience.id,
+          version: audience.version,
+        };
+      }),
+    };
+    this._save();
+  }
+
+  getAudienceCache() {
+    return this.cachedAudiences;
+  }
+
+  getAudienceCacheMetaData() {
+    return this.cachedAudiencesMetaData;
+  }
+}
+
+export const audienceCacheStore = new AudienceCacheStore();
