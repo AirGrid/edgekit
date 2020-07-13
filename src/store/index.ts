08efd4a3a243f12dbfe09248fd5c040d7c0f42e7
Peter Kirkham <k11kirky@gmail.com>
2020-07-13 19:07:25 +0100

    updated audience cache to update & renamed some bits

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-export { matchedAudienceStore } from './matchedAudiences';
->>>>>>> renamed audiences to matchedAudience store and updated naming
 export { viewStore } from './pageview';
 export { matchedAudienceStore } from './matchedAudiences';
 export { cachedAudienceStore } from './cachedAudiences';
