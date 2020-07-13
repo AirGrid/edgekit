bbc1c2201d5f3b1896ad8cfe0c3acee254bb4b37
Peter Kirkham <k11kirky@gmail.com>
2020-07-13 14:57:17 +0100

    renamed audiences to matchedAudience store and updated naming

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+export { matchedAudienceStore } from './matchedAudiences';
+>>>>>>> renamed audiences to matchedAudience store and updated naming
 export { viewStore } from './pageview';
 export { matchedAudienceStore } from './matchedAudiences';
 export { cachedAudienceStore } from './cachedAudiences';
