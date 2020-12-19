cd1bd0be262717494b331e3660199cc081410d25
Cody Welsh <codyw@protonmail.com>
2020-12-18 21:12:24 -0800

    refactor(engine/index): Efficiency suggestions
    Reduce the impact of multiple loops by invoking only one loop to check
    conditional logic.

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -11,13 +11,31 @@ const check = (
   pageViews: PageView[],
   any = false
 ): boolean => {
-  const checkedConditions = conditions.map((condition) =>
-    evaluateCondition(condition, pageViews)
-  );
+  // Instead of going through the entire array again with an `.includes`
+  // method call, let's check _during_ other operations. This has a
+  // potential savings of `n` | n => conditions.length.
+  if (any) {
+    for (const condition of conditions) {
+      if (evaluateCondition(condition, pageViews)) {
+        return true;
+      }
+    }
+  }
 
-  return any
-    ? checkedConditions.includes(true)
-    : !checkedConditions.includes(false);
+  // If we need all of the pageViews to satisfy `condition`
+  const conditionsTotal = conditions.length;
+  let conditionsMet = 0;
+  for (const condition of conditions) {
+    if (conditionsMet === conditionsTotal) {
+      return true;
+    }
+    if (evaluateCondition(condition, pageViews)) {
+      conditionsMet += 1;
+    }
+  }
+
+  // Our conditions were not met, so we default to failure
+  return false;
 };
 
 export { translate, check };
