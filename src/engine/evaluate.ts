58ea31d9785b33296dda5bddc4cce856ae384249
naripok <fernando.canteruccio@gmail.com>
2020-12-20 11:54:50 -0300

    Simplify engineEvaluate implementation

--- src/engine/evaluate.ts
+++ src/engine/evaluate.ts
@@ -7,26 +7,6 @@ import {
   AudienceDefinitionFilter,
 } from '../../types';
 
-/* Checks each pageView once for any matching queries
- * and returns the filtered array containing the matched
- * pageViews
- *
- * TODO Here, also, we have an opportunity to implement the
- * internal AND logic, swapping some for every
- * according to the value of filter.any
- */
-const filterPageViews = (
-  filter: EngineCondition<AudienceDefinitionFilter>['filter'],
-  pageViews: Readonly<PageView[]>
-): PageView[] => {
-  return pageViews.filter((pageView) =>
-    filter.queries.some((query) => {
-      const pageFeatures = pageView.features[query.queryProperty];
-      return queryMatches(query, pageFeatures);
-    })
-  );
-};
-
 /* Filter the pageView array by matching queries
  * and evaluates if it matches the conditions
  * based on the condition reducing rules
@@ -37,7 +17,35 @@ export const evaluateCondition = (
 ): boolean => {
   const { filter, rules } = condition;
 
-  const filteredPageViews = filterPageViews(filter, pageViews);
+  // if no queries, do not match at all
+  if (filter.queries.length === 0) return false;
+
+  /* Checks each pageView once for any matching queries
+   * and returns the filtered array containing the matched
+   * pageViews
+   *
+   * TODO Here, also, we have an opportunity to implement the
+   * internal AND logic, swapping some for every
+   * according to the value of filter.any
+   *
+   * NOTE: there is actually a semantic problem here.
+   * The filter definition specifies an 'any' switch,
+   * which defaults to false. However, the current
+   * filtering uses `some` to check the query conditions.
+   * The complete implementation here would be:
+   * ```
+   * filter.queries.[filter.any ? 'some' : 'every']((query) => ...
+   * ```
+   * Yet, there is too much (testing) code depending on
+   * this defaulting to `some` and the refactor would take
+   * some time.
+   */
+  const filteredPageViews = pageViews.filter((pageView) =>
+    filter.queries.some((query) => {
+      const pageFeatures = pageView.features[query.queryProperty];
+      return queryMatches(query, pageFeatures);
+    })
+  );
 
   return rules.every((rule) => {
     // TODO: allow other reducers...
@@ -48,8 +56,4 @@ export const evaluateCondition = (
 
     return matches(value);
   });
-};
-
-export const testables = {
-  filterPageViews,
 };
