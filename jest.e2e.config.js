7ea76edc84d3cb838b0a0c9df99fec1de5d88bee
naripok <fernando.canteruccio@gmail.com>
2021-01-20 16:16:28 -0300

    e2e basic run test e2e/unit configs

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -2,19 +2,14 @@ module.exports = {
   verbose: true,
   preset: 'jest-playwright-preset',
   testMatch: [
-    '**/__tests__/**/*.+(ts|js)',
-    '**/test/e2e/?(*.)+(spec|test).+(ts|js)',
+    '**/test/e2e/**/?(*.)+(test).+(ts|js)',
   ],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   testEnvironmentOptions: {
     'jest-playwright': {
-      browsers: ['firefox'],
-      exitOnPageError: false, // GitHub currently throws errors
-      lauchOptions: {
-        headless: true,
-      },
+      browsers: ['chromium', 'firefox'],
     },
   },
 };
