72e92593ace966ef0408c94643e38248915a56ed
naripok <fernando.canteruccio@gmail.com>
2021-03-10 14:19:11 -0300

    Update run and gdpr unit tests

--- test/unit/gdpr.test.ts
+++ test/unit/gdpr.test.ts
@@ -3,106 +3,116 @@ import { checkConsentStatus, waitForTcfApiTimeout } from '../../src/gdpr';
 import { TCData } from '../../types';
 import {
   makeAudienceDefinition,
-  makeStringArrayQuery,
+  makeLogisticRegressionQuery,
 } from '../helpers/audienceDefinitions';
 import { getPageViews, getMatchedAudiences } from '../helpers/localStorage';
 
-const airgridVendorId = 782;
+describe('EdgeKit GDPR tests', () => {
+  const airgridVendorId = 782;
 
-let TCData: TCData = {
-  cmpStatus: 'loaded',
-  eventStatus: 'tcloaded',
-  gdprApplies: true,
-  vendor: { consents: { [airgridVendorId]: false } },
-};
-
-let nextListenerId = 0;
-let callbacks: {
-  listenerId: number;
-  cb: (tcData: TCData, success: boolean) => void;
-}[] = [];
-
-const updateTCData = (newTCData: {
-  eventStatus?: 'tcloaded' | 'cmpuishown' | 'useractioncomplete';
-  vendor?: { consents: { [vendorId: number]: boolean | undefined } };
-  gdprApplies?: boolean;
-}) => {
-  TCData = {
-    ...TCData,
-    ...newTCData,
+  let TCData: TCData = {
+    cmpStatus: 'loaded',
+    eventStatus: 'tcloaded',
+    gdprApplies: true,
+    vendor: { consents: { [airgridVendorId]: false } },
   };
 
-  callbacks.forEach(({ listenerId, cb }) => {
-    cb(
-      {
-        ...TCData,
-        listenerId,
-      },
-      true
-    );
-  });
-};
+  let nextListenerId = 0;
+  let callbacks: {
+    listenerId: number;
+    cb: (tcData: TCData, success: boolean) => void;
+  }[] = [];
 
-function __tcfapi(
-  command: 'addEventListener',
-  version: number,
-  cb: (tcData: TCData, success: boolean) => void
-): void;
+  const updateTCData = (newTCData: {
+    eventStatus?: 'tcloaded' | 'cmpuishown' | 'useractioncomplete';
+    vendor?: { consents: { [vendorId: number]: boolean | undefined } };
+    gdprApplies?: boolean;
+  }) => {
+    TCData = {
+      ...TCData,
+      ...newTCData,
+    };
 
-function __tcfapi(
-  command: 'removeEventListener',
-  _: number,
-  cb: (success: boolean) => void
-): void;
+    callbacks.forEach(({ listenerId, cb }) => {
+      cb(
+        {
+          ...TCData,
+          listenerId,
+        },
+        true
+      );
+    });
+  };
 
-// TODO: Jest doesn't recognize function overloading
-function __tcfapi(
-  command: 'addEventListener' | 'removeEventListener',
-  _: number,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  cb: any,
-  listenerId?: number
-) {
-  if (command === 'addEventListener') {
-    const callback = { cb, listenerId: nextListenerId++ };
-    callbacks.push(callback);
-    updateTCData({});
-  } else if (command === 'removeEventListener' && listenerId) {
-    callbacks = callbacks.filter((cb) => cb.listenerId !== listenerId);
+  function __tcfapi(
+    command: 'addEventListener',
+    version: number,
+    cb: (tcData: TCData, success: boolean) => void
+  ): void;
+
+  function __tcfapi(
+    command: 'removeEventListener',
+    _: number,
+    cb: (success: boolean) => void
+  ): void;
+
+  // TODO: Jest doesn't recognize function overloading
+  function __tcfapi(
+    command: 'addEventListener' | 'removeEventListener',
+    _: number,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    cb: any,
+    listenerId?: number
+  ) {
+    if (command === 'addEventListener') {
+      const callback = { cb, listenerId: nextListenerId++ };
+      callbacks.push(callback);
+      updateTCData({});
+    } else if (command === 'removeEventListener' && listenerId) {
+      callbacks = callbacks.filter((cb) => cb.listenerId !== listenerId);
+    }
   }
-}
 
-const injectTcfApi = () => {
-  window.__tcfapi = __tcfapi;
-};
+  const injectTcfApi = () => {
+    window.__tcfapi = __tcfapi;
+  };
 
-const updateTCDataAfterDelay = (): Promise<[number, number]> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      updateTCData({
-        eventStatus: 'useractioncomplete',
-        vendor: { consents: { [airgridVendorId]: true } },
-      });
-      resolve(process.hrtime());
-    }, 500);
+  const updateTCDataAfterDelay = (): Promise<[number, number]> => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        updateTCData({
+          eventStatus: 'useractioncomplete',
+          vendor: { consents: { [airgridVendorId]: true } },
+        });
+        resolve(process.hrtime());
+      }, 500);
+    });
+  };
+
+  const featureVector = [1, 1, 1];
+
+  const audienceDefinition = makeAudienceDefinition({
+    id: 'audience_id',
+    lookBack: 10,
+    occurrences: 0,
+    definition: [
+      makeLogisticRegressionQuery({
+        queryValue: {
+          vector: featureVector,
+          bias: 0,
+          threshold: 0.9,
+        },
+      }),
+    ],
   });
-};
 
-const sportAudience = makeAudienceDefinition({
-  id: 'sport_id',
-  lookBack: 10,
-  occurrences: 0,
-  definition: [makeStringArrayQuery(['sport'])],
-});
+  const pageFeatures = {
+    docVector: {
+      version: 1,
+      value: featureVector,
+    },
+  };
 
-const sportPageFeature = {
-  keywords: {
-    version: 1,
-    value: ['sport'],
-  },
-};
-
-describe.only('EdgeKit GDPR tests', () => {
   describe('checkForConsent', () => {
     it('should fail to consent if the Transparency and Consent Framework API is missing', async () => {
       jest.setTimeout(waitForTcfApiTimeout + 500);
@@ -152,8 +162,8 @@ describe.only('EdgeKit GDPR tests', () => {
       ] = await Promise.all([
         (async () => {
           await edkt.run({
-            pageFeatures: sportPageFeature,
-            audienceDefinitions: [sportAudience],
+            pageFeatures: pageFeatures,
+            audienceDefinitions: [audienceDefinition],
             vendorIds: [airgridVendorId],
           });
           return process.hrtime();
@@ -171,14 +181,14 @@ describe.only('EdgeKit GDPR tests', () => {
       expect(edktPageViews).toEqual([
         {
           ts: edktPageViews[0].ts,
-          features: sportPageFeature,
+          features: pageFeatures,
         },
       ]);
 
       expect(edktMatchedAudiences).toEqual([
         {
           ...edktMatchedAudiences[0],
-          id: 'sport_id',
+          id: 'audience_id',
           matchedOnCurrentPageView: true,
         },
       ]);
