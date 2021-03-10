1b6593bb5e89bfc07bceb3721577c32c89a4b661
naripok <fernando.canteruccio@gmail.com>
2021-03-10 14:41:21 -0300

    Remove keywords references from leftover tests

--- test/unit/features/addFeatures.test.ts
+++ test/unit/features/addFeatures.test.ts
@@ -6,13 +6,9 @@ describe('edkt viewStore add behaviour', () => {
   const omitGdprConsent = true;
 
   const features = {
-    keywords: {
+    docVector: {
       version: 1,
-      value: ['sport', 'soccer', 'football'],
-    },
-    someVector: {
-      version: 2,
-      value: [0.4, 0.2, 0.1, 0.8],
+      value: [1, 1, 1],
     },
   };
 
