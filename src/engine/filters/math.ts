c4067ea4b1050468368a2be64a146b1ea503e07e
ydennisy <dennis@airgrid.io>
2022-02-18 09:44:37 +0000

    feat: add scaling of cosine sim to reflect real world distribution

--- src/engine/filters/math.ts
+++ src/engine/filters/math.ts
@@ -1,19 +1,28 @@
-export const dotProduct = (vectorA: number[], vectorB: number[]): number => {
-  return vectorA.reduce((acc, cur, idx) => acc + cur * vectorB[idx], 0);
-};
-
 const euclideanLength = (vector: number[]): number => {
   return Math.sqrt(vector.reduce((acc, cur) => acc + Math.pow(cur, 2), 0));
+};
+
+const cosineScaler = (x: number): number => {
+  const MAX = 0.9;
+  const MIN = -0.5;
+  const scaled = (x - MIN) / (MAX - MIN);
+  if (scaled >= 1) return 1;
+  if (scaled <= 0) return 0;
+  return scaled;
+};
+
+export const dotProduct = (vectorA: number[], vectorB: number[]): number => {
+  return vectorA.reduce((acc, cur, idx) => acc + cur * vectorB[idx], 0);
 };
 
 export const cosineSimilarity = (
   vectorA: number[],
   vectorB: number[]
 ): number => {
-  return (
+  const cosineSim =
     dotProduct(vectorA, vectorB) /
-    (euclideanLength(vectorA) * euclideanLength(vectorB))
-  );
+    (euclideanLength(vectorA) * euclideanLength(vectorB));
+  return cosineScaler(cosineSim);
 };
 
 export const sigmoid = (z: number): number => {
